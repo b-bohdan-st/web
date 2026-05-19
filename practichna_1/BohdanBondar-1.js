08b77576da594e29f4af2a1f84c3500c80882db9
Bohdan <personal.bohdan@gmail.com>
2026-05-19 13:22:07 +0300

    Update BohdanBondar-1.js

--- practichna_1/BohdanBondar-1.js
+++ practichna_1/BohdanBondar-1.js
@@ -4,6 +4,6 @@ function func1() {
     const formatted_price = UAH_price
         .toLocaleString('uk-UA')
         .replace(/\u00A0/g, ' ');
-    // alert(`Після виконання проєкту, буде нараховано ${formatted_price} грн з урахуванням комісії банку!`);
+    alert(`Після виконання проєкту, буде нараховано ${formatted_price} грн з урахуванням комісії банку!`);
     console.log(`Після виконання проєкту, буде нараховано ${formatted_price} грн з урахуванням комісії банку!`);
 }
